5a3ae1e522ccd5845c036d8fe655bc667fd18976
thegrims <grimshawaidan@gmail.com>
2016-03-17 21:38:19 -0700

    changed model

---
+++ Roll a Ball/Assets/scripts/trigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ball"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            //SceneManager.LoadScene()
+        }
+    }
+}
